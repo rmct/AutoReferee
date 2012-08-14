9971cbce4fb16cec702d106acdd4af6def251cc4
Chad Waters <authorblues@gmail.com>
2012-08-14 01:15:05 -0400

    Cleaning up report.js, simplifying jQuery selectors

--- src/main/resources/webstats/report.js
+++ src/main/resources/webstats/report.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
-  $("input[type=checkbox].player-toggle").attr('checked', true).change(function(){
+  $(".player-toggle").attr('checked', true).change(function(){
     $('tr.transcript-event.type-player-event').hide();
-    $("input[type=checkbox].player-toggle").each(function(i, e){
+    $(".player-toggle").each(function(i, e){
       if ($(e).is(':checked'))
         $('tr.transcript-event.type-player-event.player-' + $(e).data('player')).show();
     });
