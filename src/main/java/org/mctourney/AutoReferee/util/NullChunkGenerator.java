5ade1ff996a2bde4e017a35e77b0056aabd3a186
Chad Waters <authorblues@gmail.com>
2012-10-02 15:54:54 -0400

    NullChunkGenerator completed

--- src/main/java/org/mctourney/AutoReferee/util/NullChunkGenerator.java
+++ src/main/java/org/mctourney/AutoReferee/util/NullChunkGenerator.java
@@ -1,8 +1,16 @@
 package org.mctourney.AutoReferee.util;
 
+import java.util.Random;
+
+import org.bukkit.World;
 import org.bukkit.generator.ChunkGenerator;
 
 public class NullChunkGenerator extends ChunkGenerator
 {
-	
+	private static final int WORLD_HEIGHT = 256;
+
+	@Override
+	public byte[][] generateBlockSections(World world, Random r, 
+		int x, int z, ChunkGenerator.BiomeGrid biomes)
+	{ return new byte[WORLD_HEIGHT / 16][]; }
 }
