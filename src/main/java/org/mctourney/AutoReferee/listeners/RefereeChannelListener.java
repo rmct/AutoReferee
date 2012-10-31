440d18ed412ad170f84ea1999d7bd60123a7840a
Chad Waters <authorblues@gmail.com>
2012-10-30 22:46:25 -0400

    Remove player from team if connecting with client mod. Fixes #61

--- src/main/java/org/mctourney/AutoReferee/listeners/RefereeChannelListener.java
+++ src/main/java/org/mctourney/AutoReferee/listeners/RefereeChannelListener.java
@@ -8,9 +8,11 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerRegisterChannelEvent;
 import org.bukkit.plugin.messaging.PluginMessageListener;
+import org.bukkit.ChatColor;
 
 import org.mctourney.AutoReferee.AutoReferee;
 import org.mctourney.AutoReferee.AutoRefMatch;
+import org.mctourney.AutoReferee.AutoRefPlayer;
 
 public class RefereeChannelListener implements PluginMessageListener, Listener
 {
@@ -34,8 +36,22 @@ public class RefereeChannelListener implements PluginMessageListener, Listener
 	@EventHandler
 	public void channelRegistration(PlayerRegisterChannelEvent event)
 	{
-		AutoRefMatch match = plugin.getMatch(event.getPlayer().getWorld());
-		if (AutoReferee.REFEREE_PLUGIN_CHANNEL.equals(event.getChannel()) && match != null
-			&& match.isReferee(event.getPlayer())) match.updateReferee(event.getPlayer());
+		Player pl = event.getPlayer();
+		AutoRefMatch match = plugin.getMatch(pl.getWorld());
+		
+		if (AutoReferee.REFEREE_PLUGIN_CHANNEL.equals(event.getChannel()) && match != null)
+		{
+			// if this is a player, complain and force them to quit their team!
+			if (match.isPlayer(pl))
+			{
+				AutoRefPlayer apl = match.getPlayer(pl);
+				for (Player ref : match.getReferees(true)) ref.sendMessage(apl.getName() + 
+					ChatColor.DARK_GRAY + " attempted to log in with a modified client!");
+				match.leaveTeam(pl, true);
+			}
+
+			// update a referee with the latest information regarding the match
+			if (match.isReferee(pl)) match.updateReferee(pl);
+		}
 	}
 }
