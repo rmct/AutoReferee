fb4cd594c0faaf11ac623e3afee268c15934d6fb
Chad Waters <authorblues@gmail.com>
2012-11-02 17:25:22 -0400

    Allow `/artp` to non-ARP players

--- src/main/java/org/mctourney/AutoReferee/AutoReferee.java
+++ src/main/java/org/mctourney/AutoReferee/AutoReferee.java
@@ -1100,8 +1100,8 @@ public class AutoReferee extends JavaPlugin
 				}
 				else
 				{
-					AutoRefPlayer apl = match.getPlayer(args[0]);
-					tplocation = TeleportationUtil.playerTeleport(apl);
+					Player pl = getServer().getPlayer(args[0]);
+					tplocation = match.isPlayer(pl) ? TeleportationUtil.entityTeleport(pl) : pl.getLocation();
 				}
 			}
 			else tplocation = TeleportationUtil.locationTeleport(match.getLastNotificationLocation());
