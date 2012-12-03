c91e004bd917f74cc59da719fec5ab40f9f26c65
Chad Waters <authorblues@gmail.com>
2012-12-02 23:19:17 -0500

    Add rotating backgrounds

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$(document).ready(function()
+{
+	var backgrounds = [
+		"images/background-1.png",
+		"images/background-2.png",
+	];
+
+	var backgroundsP = new Array();
+	for (var i = 0; i < backgrounds.length; ++i)
+	{
+		var img = backgroundsP[i] = new Image;
+		img.src = backgrounds[i];
+	}
+
+	var cimg = 0;
+	function nextImage()
+	{
+		$('#background-image').animate({opacity: 0}, 500, function()
+		{
+			cimg = (cimg + 1) % backgroundsP.length;
+			$(this).css('background-image', 'url(' + backgrounds[cimg] + ')').animate({opacity: 1}, 500);
+		});
+	}
+
+	var cycleTask = setInterval(nextImage, 10000);
+});
