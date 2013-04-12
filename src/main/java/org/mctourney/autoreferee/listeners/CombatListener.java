a6f91f0cea7ab45a5604dc0191efdf9147152418
Keanu Vestil <keanu26@gmail.com>
2013-04-11 20:42:46 -0700

    Forgot to import this
    derp

--- src/main/java/org/mctourney/autoreferee/listeners/CombatListener.java
+++ src/main/java/org/mctourney/autoreferee/listeners/CombatListener.java
@@ -11,6 +11,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
+import org.bukkit.entity.PigZombie;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
