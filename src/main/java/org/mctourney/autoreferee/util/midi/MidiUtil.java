3c18f322e5860726ed22945e99624ccb65c2f221
Chad Waters <authorblues@gmail.com>
2013-06-03 21:19:57 -0400

    Add midi program change support (instruments)

--- src/main/java/org/mctourney/autoreferee/util/midi/MidiUtil.java
+++ src/main/java/org/mctourney/autoreferee/util/midi/MidiUtil.java
@@ -9,6 +9,7 @@ import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
 
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 
 /**
@@ -46,5 +47,42 @@ public class MidiUtil
 	public static boolean playMidiQuietly(File file, Set<Player> listeners)
 	{
 		return playMidiQuietly(file, 1.0f, listeners);
+	}
+
+	// provided by github.com/sk89q/craftbook
+	private static final int[] instruments = {
+		0, 0, 0, 0, 0, 0, 0, 5, //   8
+		6, 0, 0, 0, 0, 0, 0, 0, //  16
+		0, 0, 0, 0, 0, 0, 0, 5, //  24
+		5, 5, 5, 5, 5, 5, 5, 5, //  32
+		6, 6, 6, 6, 6, 6, 6, 6, //  40
+		5, 5, 5, 5, 5, 5, 5, 2, //  48
+		5, 5, 5, 5, 0, 0, 0, 0, //  56
+		0, 0, 0, 0, 0, 0, 0, 0, //  64
+		0, 0, 0, 0, 0, 0, 0, 0, //  72
+		0, 0, 0, 0, 0, 0, 0, 0, //  80
+		0, 0, 0, 0, 0, 0, 0, 0, //  88
+		0, 0, 0, 0, 0, 0, 0, 0, //  96
+		0, 0, 0, 0, 0, 0, 0, 0, // 104
+		0, 0, 0, 0, 0, 0, 0, 0, // 112
+		1, 1, 1, 3, 1, 1, 1, 5, // 120
+		1, 1, 1, 1, 1, 2, 4, 3, // 128
+	};
+
+	public static Sound patchToInstrument(int patch)
+	{
+		// look up the instrument matching the patch
+		switch (instruments[patch])
+		{
+			case 1: return Sound.NOTE_BASS_GUITAR;
+			case 2: return Sound.NOTE_SNARE_DRUM;
+			case 3: return Sound.NOTE_STICKS;
+			case 4: return Sound.NOTE_BASS_DRUM;
+			case 5: return Sound.NOTE_PLING;
+			case 6: return Sound.NOTE_BASS;
+		}
+
+		// if no instrument match is found, use piano
+		return Sound.NOTE_PIANO;
 	}
 }
