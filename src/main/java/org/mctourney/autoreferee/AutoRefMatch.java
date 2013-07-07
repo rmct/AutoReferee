3279bdaafb11e1e0747f6f011c320c258a6b86da
Chad Waters <authorblues@gmail.com>
2013-07-07 10:56:54 -0400

    Import FileNotFoundException

--- src/main/java/org/mctourney/autoreferee/AutoRefMatch.java
+++ src/main/java/org/mctourney/autoreferee/AutoRefMatch.java
@@ -4,6 +4,7 @@ import java.awt.image.RenderedImage;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
