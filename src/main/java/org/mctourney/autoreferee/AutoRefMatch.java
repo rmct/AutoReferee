199f75880a115036c02d47703a4448d0c256173b
Keanu Vestil <keanu26@gmail.com>
2013-09-02 00:23:27 +0100

    Why does IntelliJ hate me? Corrected the rest of the whitespace

--- src/main/java/org/mctourney/autoreferee/AutoRefMatch.java
+++ src/main/java/org/mctourney/autoreferee/AutoRefMatch.java
@@ -2323,7 +2323,7 @@ public class AutoRefMatch implements Metadatable
 				if (this.inStartRegion(spec.getLocation()))
 					spec.teleport(specSpawn.getLocation());
 
-        for (Player spec : this.getSpectators())
+		for (Player spec : this.getSpectators())
 		{ spec.getInventory().setItem(0, new ItemStack(345)); spec.getInventory().setItem(1, new ItemStack(288)); }
 
 		// set the current state to playing
